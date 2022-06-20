d1c527e5781640d3f4341632c01fe575ddf419fa
V-a-l-a-ry <104847792+V-a-l-a-ry@users.noreply.github.com>
2022-06-20 19:23:19 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace switchStatement{
+    class Program{
+        static void Main(string[] args){
+            int theval=50;
+            switch(theval){
+                case 50:
+                Console.WriteLine("theval is 50");
+                break;
+                case 51:
+                Console.WriteLine("theval is 51");
+                break;
+                case 52:
+                case 53:
+                case 54:
+                Console.WriteLine("theval is between 52 and 54");
+                break;
+            }
+        }
+    }
+}
